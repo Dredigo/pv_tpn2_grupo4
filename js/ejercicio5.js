58ea617b23d8fef92a242bf89293939e53befce5
Nicolas Herrera <nkhj300gmeil.com@gmail.com>
2025-04-24 16:01:25 -0300

    Ejercicio 5: Conversión de horas y minutos a segundos

---
+++ js/ejercicio5.js
@@ -0,0 +1,12 @@
+/*Declarar dos variables denominadas horas, minutos, asignar valores a cada una y
+realizar la conversión para expresar esas horas y minutos en segundos. Mostrar por consola
+las horas y minutos, luego el resultado de la conversión en segundos.*/
+
+let horas = 2;
+let minutos = 30;
+let segundos = (horas * 3600) + (minutos * 60);
+console.log(horas + " horas y " + minutos + " minutos equivalen a " + segundos + " segundos.");
+
+
+
+
