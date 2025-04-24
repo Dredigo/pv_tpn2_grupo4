165f2163fceaf2e6a08d199e613236ddb3d9f057
Nicolas Herrera <nkhj300gmeil.com@gmail.com>
2025-04-24 16:13:19 -0300

    Ejercicio 11: Cálculo de consumo de combustible por kilómetro

---
+++ js/ejercicio11.js
@@ -0,0 +1,9 @@
+/*Solicitar al usuario que ingrese el número de kilómetros recorridos por su vehículo y el
+número de litros de combustible consumidos. Calcular y mostrar el consumo de
+combustible por kilómetro.
+ El resultado se obtiene dividiendo número de litros dividido por número de kilómetros.*/
+
+let kilometros = 100;
+let litros = 10;
+let consumo = litros / kilometros;
+console.log("El consumo de combustible por kilómetro es de " + consumo + " litros.");
